b94182356e964246b7e2f69b85ae0933ac304a12
FLOUBCKF <161651558+FLOUBCKF@users.noreply.github.com>
2024-08-15 16:49:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [
-    {
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
         enunciado: "Pergunta 1",
-alternativas: [
-    "Alternativa 1",
-    "Alternativa 2"
-]
+        alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1","Alternativa 2"]
     }
+    
 ]
+
+let atual = 0;
+let pergunaAtual;
+
+function mostraPerguntas(){
+    pergunaAtual = perguntas[atual];
+    caixaPerguntas.textContent = pergunaAtual
+}
